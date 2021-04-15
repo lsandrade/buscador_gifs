be01900dd548732ce32f76ce7ae24bfc77dc032e
Luan Andrade <luansandrade92@hotmail.com>
2021-04-14 23:13:02 -0300

    Request giphys online

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const base_url = "https://api.giphy.com/v1/gifs";
+const api_key = String.fromEnvironment('API_KEY', defaultValue: 'API_KEY');
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +12,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    String url = (_search == null)
+        ? "$base_url/trending?api_key=$api_key&limit=20&rating=g"
+        : "$base_url/search?api_key=$api_key&q=$_search&limit=20&offset=$_offset&rating=g&lang=en";
+
+    http.Response response = await http.get(url);
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
